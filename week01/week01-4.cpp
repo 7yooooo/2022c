a36c063fde7097ad304e2b807a21bb38745cf995
jsyeh <jsyeh@mail.mcu.edu.tw>
2023-03-03 08:54:01 +0800

    修改目錄、新增Week01

---
+++ week01/week01-4.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#intclude<string.h>
+int main()
+{
+
+    char line[40];
+    int total=0;
+    scanf("%s",line);
+    int ans=0;
+    int N=strlen(line);
+    for(int i=0;i<=n;i++)
+    {
+
+        if(line[i]=='2');
+        ans++;
+
+    }
+
+    printf("%d\n",ans);
+    total+=ans;
+}
---
+++ week01/week01-5.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#intclude<string.h>
+int main()
+{
+
+    char line[40];
+    int total=0;
+    while(scanf("%s",line)==1)
+    {
+        int ans=0;
+        int N=strlen(line);
+        for(int i=0;i<N;i++)
+        {
+            if(line[i]=='2');
+            ans++;
+
+
+        }
+
+        printf("%d\n",ans);
+        total+=ans;
+    }
+    printf("Total: %d\n",total);
+}
+
+
